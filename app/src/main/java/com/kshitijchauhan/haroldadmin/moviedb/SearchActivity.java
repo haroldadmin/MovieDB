900c8fb20692d252c1629080056b209aa1280d8f
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2018-04-02 11:05:35 +0530

    Initialize repository

---
+++ app/src/main/java/com/kshitijchauhan/haroldadmin/moviedb/SearchActivity.java
@@ -0,0 +1,13 @@
+package com.kshitijchauhan.haroldadmin.moviedb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
