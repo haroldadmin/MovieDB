b1623b5679152de3a9abf96a643775b62cbf0d97
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2018-04-10 21:05:25 +0530

    Switched to recycler view and cards in search results page

--- app/src/main/java/com/kshitijchauhan/haroldadmin/moviedb/MovieDetailsActivity.java
+++
@@ -1,5 +0,0 @@
-package com.kshitijchauhan.haroldadmin.moviedb;
-
-public class MovieDetailsActivity {
-
-}
--- app/src/main/java/com/kshitijchauhan/haroldadmin/moviedb/SearchResultsAdapter.java
+++ app/src/main/java/com/kshitijchauhan/haroldadmin/moviedb/SearchResultsAdapter.java
@@ -1,88 +1,76 @@
 package com.kshitijchauhan.haroldadmin.moviedb;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.media.Image;
-import android.os.AsyncTask;
+import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.util.Log;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 
-public class SearchResultsAdapter extends ArrayAdapter<Movie> {
+public class SearchResultsAdapter extends RecyclerView.Adapter<SearchResultsAdapter.ViewHolder> {
 
-    public static final String LOG_TAG = SearchResultsAdapter.class.getName();
+    private ArrayList<Movie> mSearchResults;
+    private Context mContext;
 
-    public SearchResultsAdapter(Context context, ArrayList<Movie> movies) {
-        super(context, 0, movies);
+    public SearchResultsAdapter(Context context, ArrayList<Movie> searchResults) {
+        mContext = context;
+        mSearchResults = searchResults;
+    }
+
+    public Context getContext() {
+        return mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View searchResultCard = LayoutInflater.from(parent.getContext()).inflate(R.layout.search_results_card, parent, false);
+        ViewHolder holder = new ViewHolder(searchResultCard);
+        return holder;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-
-        View listItemView = convertView;
-        ViewHolder holder;
-        Movie searchResult = getItem(position);
-        if (listItemView == null) {
-            holder = new ViewHolder();
-            listItemView = LayoutInflater.from(getContext()).inflate(
-                    R.layout.search_results_list_item, parent, false);
-            holder.name = listItemView.findViewById(R.id.movie_name);
-            holder.year = listItemView.findViewById(R.id.year);
-            holder.poster = listItemView.findViewById(R.id.poster);
-            listItemView.setTag(holder);
-        }
-        else {
-            holder = (ViewHolder) convertView.getTag();
-        }
-
-        holder.name.setText(searchResult.getName());
-        holder.year.setText(searchResult.getYear());
-        holder.poster.setImageBitmap(searchResult.getPoster());
-        return listItemView;
-    }
-    private class ImageLoadTask extends AsyncTask<URL, Void, Bitmap> {
-        @Override
-        protected Bitmap doInBackground(URL... urls) {
-            Bitmap myBitmap = null;
-            URL url = urls[0];
-            try {
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setDoInput(true);
-                connection.connect();
-                InputStream input = connection.getInputStream();
-                myBitmap = BitmapFactory.decodeStream(input);
-                Log.e("Bitmap", "returned");
-                return myBitmap;
-            } catch (IOException e) {
-                e.printStackTrace();
-                Log.e("Exception", e.getMessage());
-            }
-            return myBitmap;
-        }
-
-        @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            super.onPostExecute(bitmap);
-        }
+    public void onBindViewHolder(@NonNull SearchResultsAdapter.ViewHolder holder, int position) {
+        Movie searchResult = (Movie) mSearchResults.get(position);
+        TextView nameTextView = holder.name;
+        TextView yearTextView = holder.year;
+        ImageView posterView = holder.poster;
+        nameTextView.setText(searchResult.getName());
+        yearTextView.setText(searchResult.getYear());
+        posterView.setImageBitmap(searchResult.getPoster());
     }
 
-    static class ViewHolder {
-        TextView name;
-        TextView year;
-        ImageView poster;
+    @Override
+    public int getItemCount() {
+        return mSearchResults.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView name;
+        public TextView year;
+        public ImageView poster;
+
+        public ViewHolder (View cardView) {
+            super(cardView);
+            name = cardView.findViewById(R.id.supporting_text);
+            year = cardView.findViewById(R.id.supporting_secondary_text);
+            poster = cardView.findViewById(R.id.media_image);
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION) {
+                        Intent intent = new Intent(mContext, MovieDetails.class);
+                        intent.putExtra("IMDb ID", mSearchResults.get(position).getImdbID());
+                        mContext.startActivity(intent);
+                    }
+                }
+            });
+        }
     }
 }
