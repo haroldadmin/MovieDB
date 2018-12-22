92f64034aeb90e74f49135c7d6fed7bd05943832
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2018-12-22 10:46:55 +0530

    Initial commit

---
+++ app/src/main/java/com/kshitijchauhan/haroldadmin/moviedb/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kshitijchauhan.haroldadmin.moviedb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/kshitijchauhan/haroldadmin/moviedb/utils/SharedPreferencesDelegate.kt
@@ -0,0 +1,50 @@
+package com.kshitijchauhan.haroldadmin.moviedb.utils
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
+import com.kshitijchauhan.haroldadmin.moviedb.R
+import kotlin.reflect.KProperty
+
+class SharedPreferencesDelegate<T>(val context: Context, val key: String, val defaultValue: T) {
+    val prefs: SharedPreferences by lazy { context.getSharedPreferences(context.getString(R.string.app_name), Context.MODE_PRIVATE) }
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        return findPreferences(key, defaultValue)
+    }
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        savePreference(key, value)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun findPreferences(key: String, defaultValue: T): T {
+        with(prefs)
+        {
+            val result: Any = when (defaultValue) {
+                is Boolean -> getBoolean(key, defaultValue)
+                is Int -> getInt(key, defaultValue)
+                is Long -> getLong(key, defaultValue)
+                is Float -> getFloat(key, defaultValue)
+                is String -> getString(key, defaultValue)
+                else -> throw IllegalArgumentException()
+            }
+            return result as T
+        }
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    private fun savePreference(key: String, value: T) {
+        with(prefs.edit())
+        {
+            when (value) {
+                is Boolean -> putBoolean(key, value)
+                is Int -> putInt(key, value)
+                is Long -> putLong(key, value)
+                is Float -> putFloat(key, value)
+                is String -> putString(key, value)
+                else -> throw IllegalArgumentException()
+            }.apply()
+        }
+    }
+}
