a3c7d522cfedea225b63e8402e415fc73ef3d80f
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2018-12-22 23:44:40 +0530

    feat: Add main activity layout

--- app/src/main/java/com/kshitijchauhan/haroldadmin/moviedb/ui/main/MainActivity.kt
+++ app/src/main/java/com/kshitijchauhan/haroldadmin/moviedb/ui/main/MainActivity.kt
@@ -3,12 +3,18 @@ package com.kshitijchauhan.haroldadmin.moviedb.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kshitijchauhan.haroldadmin.moviedb.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(mainToolbar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+        }
 
         savedInstanceState ?: supportFragmentManager
             .beginTransaction()
