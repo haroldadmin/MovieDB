264e319bda171ee801cea7fd5c82d3719d55aa55
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2018-12-23 12:58:24 +0530

    feat: Tweak replaceFragment extension to not repeat tansactions

--- app/src/main/java/com/kshitijchauhan/haroldadmin/moviedb/utils/Extensions.kt
+++ app/src/main/java/com/kshitijchauhan/haroldadmin/moviedb/utils/Extensions.kt
@@ -5,17 +5,44 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.kshitijchauhan.haroldadmin.moviedb.BuildConfig
 import com.kshitijchauhan.haroldadmin.moviedb.MovieDBApplication
+import com.kshitijchauhan.haroldadmin.moviedb.ui.BaseFragment
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 
 fun AppCompatActivity.app(): MovieDBApplication = this.application as MovieDBApplication
 
-fun AppCompatActivity.log(message: String) {
+fun Any.log(message: String) {
     if (BuildConfig.DEBUG) Log.d(this::class.java.simpleName, message)
 }
 
-fun Fragment.log(message: String) {
-    if (BuildConfig.DEBUG) Log.d(this::class.java.simpleName, message)
+/**
+ * This method is used to replace the current fragment in the Activity
+ * with the given new Fragment. It has a safeguard against adding the same
+ * fragment twice: It does not perform the fragment transaction if the new type
+ * of the new fragment is the same as the type of the already existing fragment.
+ *
+ * @param fragment The new fragment to display
+ * @param containerId The ID of the container in which to place this fragment
+ * @param addToBackStack To indicate whether this fragment should be added to
+ * the backstack
+ * @param backStackName The name to give when adding to backstack
+ */
+inline fun <reified T: BaseFragment> AppCompatActivity.replaceFragment(fragment: T, containerId: Int, addToBackStack: Boolean = true, backStackName: String? = null) {
+    val currentFragment = this.supportFragmentManager
+        .findFragmentById(containerId)
+
+    if (currentFragment is T) {
+        this.log("Not replacing the current fragment of type ${T::class.java.simpleName}")
+        return
+    }
+
+    val ft = this.supportFragmentManager
+        .beginTransaction()
+        .replace(containerId, fragment)
+    if (addToBackStack) {
+        ft.addToBackStack(backStackName)
+    }
+    ft.commit()
 }
 
 fun Disposable.disposeWith(compositeDisposable: CompositeDisposable) = compositeDisposable.add(this)
