c7e08179f4a83cfac3be0cd434088cdec7cf4402
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2018-12-27 16:17:38 +0530

    feat: Switch to bottom navigation

--- app/src/main/java/com/kshitijchauhan/haroldadmin/moviedb/ui/details/MovieDetailsFragment.kt
+++ app/src/main/java/com/kshitijchauhan/haroldadmin/moviedb/ui/details/MovieDetailsFragment.kt
@@ -1,34 +1,20 @@
 package com.kshitijchauhan.haroldadmin.moviedb.ui.details
 
-import android.graphics.Bitmap
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import androidx.palette.graphics.Palette
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.bitmap.BitmapTransitionOptions
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.request.target.Target
 import com.kshitijchauhan.haroldadmin.moviedb.R
 import com.kshitijchauhan.haroldadmin.moviedb.remote.service.movie.Movie
 import com.kshitijchauhan.haroldadmin.moviedb.ui.BaseFragment
 import com.kshitijchauhan.haroldadmin.moviedb.utils.Constants
-import com.kshitijchauhan.haroldadmin.moviedb.utils.disposeWith
-import com.kshitijchauhan.haroldadmin.moviedb.utils.snackbar
-import io.reactivex.Single
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_movie_details.*
 
 
--- app/src/main/java/com/kshitijchauhan/haroldadmin/moviedb/ui/main/MainActivity.kt
+++ app/src/main/java/com/kshitijchauhan/haroldadmin/moviedb/ui/main/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_alternate)
 
         mainViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
 
