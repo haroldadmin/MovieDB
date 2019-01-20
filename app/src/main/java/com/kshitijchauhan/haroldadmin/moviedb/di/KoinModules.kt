a1b37c760a94268fc201ac59597c0e6747750be2
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-01-20 16:35:42 +0530

    feat: Add Koin modules

---
+++ app/src/main/java/com/kshitijchauhan/haroldadmin/moviedb/di/KoinModules.kt
@@ -0,0 +1,99 @@
+package com.kshitijchauhan.haroldadmin.moviedb.di
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.kshitijchauhan.haroldadmin.moviedb.BuildConfig
+import com.kshitijchauhan.haroldadmin.moviedb.R
+import com.kshitijchauhan.haroldadmin.moviedb.remote.ApiKeyInterceptor
+import com.kshitijchauhan.haroldadmin.moviedb.remote.ApiManager
+import com.kshitijchauhan.haroldadmin.moviedb.remote.Config
+import com.kshitijchauhan.haroldadmin.moviedb.remote.SessionIdInterceptor
+import com.kshitijchauhan.haroldadmin.moviedb.remote.service.account.AccountService
+import com.kshitijchauhan.haroldadmin.moviedb.remote.service.auth.AuthenticationService
+import com.kshitijchauhan.haroldadmin.moviedb.remote.service.discover.DiscoveryService
+import com.kshitijchauhan.haroldadmin.moviedb.remote.service.movie.MovieService
+import com.kshitijchauhan.haroldadmin.moviedb.remote.service.search.SearchService
+import com.kshitijchauhan.haroldadmin.moviedb.ui.common.BottomNavManager
+import com.kshitijchauhan.haroldadmin.moviedb.ui.common.ProgressBarManager
+import com.kshitijchauhan.haroldadmin.moviedb.utils.Constants
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.parameter.parametersOf
+import org.koin.dsl.module.module
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+
+val applicationModule = module {
+    single<SharedPreferences> {
+        androidContext().getSharedPreferences(
+            androidContext().getString(R.string.app_name),
+            Context.MODE_PRIVATE
+        )
+    }
+}
+
+val retrofitModule = module {
+    single { Cache(androidContext().cacheDir, 50 * 1024 * 1024) }
+
+    single { (sessionId: String) -> SessionIdInterceptor(sessionId) }
+
+    single { (apiKey: String) -> ApiKeyInterceptor(apiKey) }
+
+    single { (loggingLevel: HttpLoggingInterceptor.Level) ->
+        HttpLoggingInterceptor().apply {
+            level = loggingLevel
+        }
+    }
+
+    single {
+
+        val apiKey = BuildConfig.API_KEY
+        val sessionId = get<SharedPreferences>().getString(Constants.KEY_SESSION_ID, "")
+
+        OkHttpClient.Builder()
+            .addInterceptor(get<ApiKeyInterceptor> {
+                parametersOf(apiKey)
+            })
+            .addInterceptor(get<SessionIdInterceptor> {
+                parametersOf(sessionId)
+            })
+            .addInterceptor(get<HttpLoggingInterceptor> {
+                parametersOf(HttpLoggingInterceptor.Level.BODY)
+            })
+            .cache(get())
+            .build()
+    }
+
+    single { MoshiConverterFactory.create() }
+
+    single { RxJava2CallAdapterFactory.create() }
+
+    single {
+        Retrofit.Builder()
+            .client(get())
+            .addCallAdapterFactory(get<RxJava2CallAdapterFactory>())
+            .addConverterFactory(get<MoshiConverterFactory>())
+            .baseUrl(Config.BASE_URL)
+            .build()
+    }
+}
+
+val apiModule = module {
+    factory { get<Retrofit>().create(AuthenticationService::class.java) }
+    factory { get<Retrofit>().create(DiscoveryService::class.java) }
+    factory { get<Retrofit>().create(SearchService::class.java) }
+    factory { get<Retrofit>().create(MovieService::class.java) }
+    factory { get<Retrofit>().create(AccountService::class.java) }
+    single { ApiManager(get(), get(), get(), get(), get()) }
+}
+
+val uiModule = module {
+    single { BottomNavManager() }
+    single { ProgressBarManager() }
+}
+
+
