9e2e3589b9b9744feb58aa1ac719bae6c7dfca7b
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-02-25 20:03:50 +0530

    refactor: Change movie credits to use a carousel

--- app/src/main/java/com/kshitijchauhan/haroldadmin/moviedb/ui/movie_details/DetailsEpoxyController.kt
+++ app/src/main/java/com/kshitijchauhan/haroldadmin/moviedb/ui/movie_details/DetailsEpoxyController.kt
@@ -1,7 +1,7 @@
 package com.kshitijchauhan.haroldadmin.moviedb.ui.movie_details
 
 import android.view.View
-import com.airbnb.epoxy.TypedEpoxyController
+import com.airbnb.epoxy.*
 import com.bumptech.glide.RequestManager
 import com.kshitijchauhan.haroldadmin.moviedb.repository.actors.Actor
 import com.kshitijchauhan.haroldadmin.moviedb.repository.data.Resource
@@ -9,7 +9,7 @@ import com.kshitijchauhan.haroldadmin.moviedb.repository.movies.AccountState
 import com.kshitijchauhan.haroldadmin.moviedb.repository.movies.Movie
 import com.kshitijchauhan.haroldadmin.moviedb.repository.movies.MovieTrailer
 import com.kshitijchauhan.haroldadmin.moviedb.ui.UIState
-import com.kshitijchauhan.haroldadmin.moviedb.ui.common.actor
+import com.kshitijchauhan.haroldadmin.moviedb.ui.common.ActorModel_
 import com.kshitijchauhan.haroldadmin.moviedb.ui.common.header
 import com.kshitijchauhan.haroldadmin.moviedb.ui.common.infoText
 import com.kshitijchauhan.haroldadmin.moviedb.ui.common.loading
@@ -18,7 +18,7 @@ import com.kshitijchauhan.haroldadmin.moviedb.utils.extensions.safe
 class DetailsEpoxyController(
     private val callbacks: DetailsEpoxyController.MovieDetailsCallbacks,
     private val glide: RequestManager
-): TypedEpoxyController<UIState.DetailsScreenState>() {
+) : TypedEpoxyController<UIState.DetailsScreenState>() {
 
     interface MovieDetailsCallbacks {
         fun toggleMovieFavouriteStatus()
@@ -30,8 +30,8 @@ class DetailsEpoxyController(
         with(state) {
             buildAccountStatesResource(accountStatesResource)
             buildMovieResource(movieResource)
-//            buildTrailerResource(trailerResource)
             buildCastResource(castResource)
+//            buildTrailerResource(trailerResource)
         }
     }
 
@@ -134,42 +134,54 @@ class DetailsEpoxyController(
     }
 
     private fun buildCastResource(actors: List<Resource<Actor>>) {
-        actors.let {
-            header {
-                id("cast")
-                title("Cast")
-                spanSizeOverride { totalSpanCount, _, _ -> totalSpanCount }
-            }
+        actors
+            .filterIsInstance<Resource.Success<Actor>>()
+            .takeIf { it.isNotEmpty() }
+            ?.let { actorsList ->
 
-            it.takeIf { list -> !list.isNullOrEmpty() }?.forEach { actorResource ->
-                when (actorResource) {
-                    is Resource.Success -> {
-                        actor {
-                            id(actorResource.data.id)
-                            actorId(actorResource.data.id)
-                            name(actorResource.data.name)
-                            glide(glide)
-                            pictureUrl(actorResource.data.profilePictureUrl)
-                            transitionName("actor-${actorResource.data.id}")
-                            clickListener { model, _, clickedView, _ ->
+                header {
+                    id("cast")
+                    title("Cast")
+                    spanSizeOverride { totalSpanCount, _, _ -> totalSpanCount }
+                }
+
+                carousel {
+                    id("cast-carousel")
+                    numViewsToShowOnScreen(this@DetailsEpoxyController.spanCount.toFloat().minus(1f).times(1.2f))
+                    withModelsFrom(actorsList) { actorResource ->
+                        ActorModel_()
+                            .id(actorResource.data.id)
+                            .actorId(actorResource.data.id)
+                            .name(actorResource.data.name)
+                            .glide(glide)
+                            .pictureUrl(actorResource.data.profilePictureUrl)
+                            .transitionName("actor-${actorResource.data.id}")
+                            .clickListener { model, _, clickedView, _ ->
                                 callbacks.onActorItemClicked(model.actorId!!, model.transitionName(), clickedView)
                             }
-                        }
                     }
-                    is Resource.Error -> {
-                        infoText {
-                            id("error-cast")
-                            text("An error occurred while trying to load the cast for this movie")
-                        }
-                    }
-                    is Resource.Loading -> Unit
-                }.safe
-
-            } ?: infoText {
-                id("cast-empty")
-                text("This list seems to be empty")
-                spanSizeOverride { totalSpanCount, _, _ -> totalSpanCount }
+                }
             }
         }
+
+    /** For use in the buildModels method of EpoxyController. A shortcut for creating a Carousel model, initializing it, and adding it to the controller.
+     *
+     */
+    private inline fun EpoxyController.carousel(modelInitializer: CarouselModelBuilder.() -> Unit) {
+        CarouselModel_().apply {
+            modelInitializer()
+        }.addTo(this)
+    }
+
+    /** Add models to a CarouselModel_ by transforming a list of items into EpoxyModels.
+     *
+     * @param items The items to transform to models
+     * @param modelBuilder A function that take an item and returns a new EpoxyModel for that item.
+     */
+    private inline fun <T> CarouselModelBuilder.withModelsFrom(
+        items: List<T>,
+        modelBuilder: (T) -> EpoxyModel<*>
+    ) {
+        models(items.map { modelBuilder(it) })
     }
 }
