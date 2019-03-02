0aa4e73432874869686c3dedff4fed023295fa39
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-03-02 23:10:49 +0530

    fix: Fix issue #6 to update settings screen toolbar title

--- app/src/main/java/com/kshitijchauhan/haroldadmin/moviedb/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/kshitijchauhan/haroldadmin/moviedb/ui/settings/SettingsFragment.kt
@@ -5,18 +5,27 @@ import android.os.Bundle
 import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 import com.kshitijchauhan.haroldadmin.moviedb.R
+import com.kshitijchauhan.haroldadmin.moviedb.ui.main.MainViewModel
 import com.kshitijchauhan.haroldadmin.moviedb.utils.Constants
 import com.kshitijchauhan.haroldadmin.moviedb.utils.SharedPreferencesDelegate
 import org.koin.android.ext.android.get
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import java.util.*
 
 class SettingsFragment : PreferenceFragmentCompat() {
+
+    private val mainViewModel: MainViewModel by sharedViewModel()
 
     private var countryName by SharedPreferencesDelegate(
         get<SharedPreferences>(),
         Constants.KEY_COUNTRY_NAME,
         Locale.getDefault().displayCountry
     )
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        mainViewModel.updateToolbarTitle("Settings")
+    }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preference_root, rootKey)
